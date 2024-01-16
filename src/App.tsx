a600722a52408c8ad05a4cebaa002c8a29c4a27a
janhandl <jh@alphainside.eu>
2024-01-16 07:13:12 +0100

    added blobs

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css'
 import {Canvas, Vector3} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import Blob from "./meshes/Blob.tsx";
+import {Slider} from "@mui/material";
+import {useState} from "react";
 
 
 
 
 function App() {
+
+    const [amount,setAmount] = useState(100);
     function between(min:number, max:number) {
         return Math.floor(
             Math.random() * (max - min) + min
@@ -16,23 +20,29 @@ function App() {
     function getRandompos(min:number, max:number):Vector3{
         return [between(min,max),between(min,max),between(min,max)]
     }
-
+    const handleSliderChange = (_event: Event, newValue: number | number[]) => {
+        setAmount(newValue as number);
+    };
   return (
-      <div>
+      <div style={{
+        width:'100vw',
+            height:'100vh',
+            }}>
+
           <Canvas style={{
               width:'100vw',
-              height:'100vh',
+              height:'80vh',
               alignItems:'center',
               justifyContent:'center',
               backgroundColor:'black'
           }} camera={{ position: [15, 15, 15] }}
           >
               <OrbitControls />
+
               {(() => {
                   const arr = [];
-                  for (let i = 0; i < 500; i++) {
+                  for (let i = 0; i < amount; i++) {
                       let randomPos = getRandompos(-40,40);
-                      console.log(randomPos);
                       arr.push(
                               <Blob position={randomPos}/>
                       );
@@ -40,6 +50,14 @@ function App() {
                   return arr;
               })()}
           </Canvas>
+          <Slider
+              style={{position:'absolute',bottom:'10%',right:'10%',width:"80%"}}
+              defaultValue={100}
+              step={100}
+              value={typeof amount === 'number' ? amount : 100}
+              onChange={handleSliderChange}
+              marks min={100} max={500}
+          />
       </div>
 
   )
